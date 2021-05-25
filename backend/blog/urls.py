5424338e5c57eb790271a0de9278681cebaac192
wasselbousmaha <wassel2005@gmail.com>
2021-05-25 15:58:20 +0200

    created path to the first blog view, get_articles

---
+++ backend/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("get_all/", views.get_articles),
+    path("get_all/<int:number>/", views.get_articles),
+    path("get_all/<int:number>/<int:offset>", views.get_articles)
+]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Article, Tag
+
+@api_view(["GET"])
+def get_articles(request, offset=0, number=20):
+    offset += 1
+    return Response(list(range(offset, offset+number)), status.HTTP_200_OK)
