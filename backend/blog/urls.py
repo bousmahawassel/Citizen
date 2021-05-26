8a8d6178e77cb556c45ef6fbf98dc03035fcadd1
wasselbousmaha <wassel2005@gmail.com>
2021-05-26 11:43:13 +0200

    added view to get an article

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("get_all/", views.get_articles),
     path("get_all/<int:number>/", views.get_articles),
-    path("get_all/<int:number>/<int:offset>", views.get_articles)
+    path("get_all/<int:number>/<int:offset>/", views.get_articles),
+    path("article/<int:id>/", views.article)
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -10,3 +10,11 @@ from .serializers import Article, Tag, ArticleSerializer, ArticleApercuSerialize
 def get_articles(request, offset=0, number=20):
     articles = Article.objects.all().order_by('date')[offset:offset+number]
     return Response(ArticleApercuSerializer(articles, many=True).data, status.HTTP_200_OK)
+
+@api_view(["GET"])
+def article(request, id):
+    try:
+        article = Article.objects.get(pk=id)
+    except:
+        return Response("article not found", status.HTTP_404_NOT_FOUND)
+    return Response(ArticleSerializer(article).data, status.HTTP_200_OK)
