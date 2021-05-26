5a853779843754e46afe2c30a52b84e651ac6326
wasselbousmaha <wassel2005@gmail.com>
2021-05-26 22:58:55 +0200

    Removed many useless elements from app

--- app/Navigator.js
+++ app/Navigator.js
@@ -1,113 +1,20 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { View, StyleSheet, Alert } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import {
-  createStackNavigator,
-  CardStyleInterpolators
-} from "@react-navigation/stack";
-import Edit from "./screens/Edit";
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 import Show from "./screens/Show";
-import New from "./screens/New";
-import Login from "./screens/Login";
 import ShowSingle from "./screens/ShowSingle";
-import IconTitle from "./components/IconTitle";
-import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
-import { logoutUser, clearPosts, firebaseEvents } from "./store/actions";
-import { firebaseAuth } from "./lib/firebase";
 
 const Stack = createStackNavigator();
 
 export default function Navigator() {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(store => store.auth.isLoggedIn);
-
-  useEffect(() => {
-    firebaseAuth.onAuthStateChanged(function(user) {
-      if (user) {
-        // user is signed in
-        dispatch(firebaseEvents());
-      } else {
-        //user is signing out
-        dispatch(clearPosts());
-      }
-    });
-  }, []);
-
-  const handleLogout = () => {
-    Alert.alert(
-      "Confirm",
-      "Are you sure you want to log out?",
-      [
-        {
-          text: "Cancel",
-          style: "cancel"
-        },
-        {
-          text: "OK",
-          onPress: () => {
-            firebaseAuth.signOut();
-            dispatch(logoutUser());
-          },
-          style: "destructive"
-        }
-      ],
-      { cancelable: false }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <>
+                    <Stack.Screen name="Show" component={Show}/>
+                    <Stack.Screen name="ShowSingle" component={ShowSingle}/>
+                </>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-  };
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={({ navigation }) => ({
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-          headerTitle: props => <IconTitle {...props} />,
-          headerStyle: {
-            backgroundColor: "#1b1c1d"
-          },
-          headerTintColor: "#fff",
-          headerTitleAlign: "center",
-          headerRight: () => (
-            <View style={styles.headerRight}>
-              <MaterialCommunityIcons
-                onPress={handleLogout}
-                name="account-minus-outline"
-                size={25}
-                color="#fff"
-              />
-              <AntDesign
-                style={{ marginLeft: 15 }}
-                name="plus"
-                color="#fff"
-                size={25}
-                onPress={() => navigation.navigate("New")}
-              />
-            </View>
-          )
-        })}
-      >
-        {!isLoggedIn ? (
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ headerRight: null }}
-          />
-        ) : (
-          <>
-            <Stack.Screen name="Show" component={Show} />
-            <Stack.Screen name="New" component={New} />
-            <Stack.Screen name="Edit" component={Edit} />
-            <Stack.Screen name="ShowSingle" component={ShowSingle} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
 }
-
-const styles = StyleSheet.create({
-  headerRight: {
-    marginRight: 10,
-    flexDirection: "row"
-  }
-});
--- app/screens/ShowSingle.js
+++ app/screens/ShowSingle.js
@@ -1,107 +1,74 @@
 import React from "react";
 import {
-  StyleSheet,
-  ScrollView,
-  Text,
-  View,
-  Image,
-  Button
+    StyleSheet,
+    ScrollView,
+    Text,
+    View,
+    Image,
+    Button
 } from "react-native";
-import { useSelector, useDispatch } from "react-redux";
-import { fbDeletePost } from "../store/actions";
 
-const ShowSingle = ({ route, navigation }) => {
-  const { item } = route.params;
-  const post = useSelector(store => store.posts[item]);
-  const dispatch = useDispatch();
+const ShowSingle = ({route, navigation}) => {
 
-  return (
-    <View style={styles.container}>
-      {typeof post != "undefined" ? (
-        <ScrollView style={styles.scroll}>
-          <View style={styles.post}>
-            <Image
-              style={styles.image}
-              source={{
-                uri: post.image
-              }}
-            />
-            <Text style={styles.title}>{post.title}</Text>
-            <Text style={styles.description}>{post.description}</Text>
-            <View style={styles.buttons}>
-              <View style={styles.singleButton}>
-                <Button
-                  color="orange"
-                  title="Edit"
-                  onPress={() => navigation.navigate("Edit", { item })}
-                />
-              </View>
-              <View style={styles.singleButton}>
-                <Button
-                  color="red"
-                  title="Delete"
-                  onPress={() => {
-                    dispatch(fbDeletePost(item)).then(() => {
-                      navigation.goBack();
-                    });
-                  }}
-                />
-              </View>
-            </View>
-          </View>
-        </ScrollView>
-      ) : (
-        <Text></Text>
-      )}
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            {typeof post != "undefined" ? (
+                <ScrollView style={styles.scroll}>
+                    <View style={styles.post}>
+                        <Image
+                            style={styles.image}
+                            source={{
+                                uri: post.image
+                            }}
+                        />
+                        <Text style={styles.title}>{post.title}</Text>
+                        <Text style={styles.description}>{post.description}</Text>
+                    </View>
+                </ScrollView>
+            ) : (
+                <Text></Text>
+            )}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center"
-  },
-  post: {
-    marginTop: 20,
-    width: "80%",
-    maxWidth: 355,
-    alignSelf: "center"
-  },
-  image: {
-    alignSelf: "center",
-    height: 180,
-    width: "100%",
-    paddingHorizontal: 10,
-    marginTop: 15
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 18,
-    paddingHorizontal: 10,
-    paddingTop: 5,
-    alignSelf: "center"
-  },
-  description: {
-    textAlign: "center",
-    paddingHorizontal: 10,
-    paddingTop: 15
-  },
-  buttons: {
-    marginTop: 15,
-    marginBottom: 10,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginBottom: 50
-  },
-  singleButton: {
-    width: 70
-  },
-  scroll: {
-    width: "100%"
-  }
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center"
+    },
+    post: {
+        marginTop: 20,
+        width: "80%",
+        maxWidth: 355,
+        alignSelf: "center"
+    },
+    image: {
+        alignSelf: "center",
+        height: 180,
+        width: "100%",
+        paddingHorizontal: 10,
+        marginTop: 15
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+        paddingHorizontal: 10,
+        paddingTop: 5,
+        alignSelf: "center"
+    },
+    description: {
+        textAlign: "center",
+        paddingHorizontal: 10,
+        paddingTop: 15
+    },
+    singleButton: {
+        width: 70
+    },
+    scroll: {
+        width: "100%"
+    }
 });
 
 export default ShowSingle;
