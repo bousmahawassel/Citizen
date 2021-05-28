f37cab9155b2197aef14849c17a3a34507559124
wasselbousmaha <wassel2005@gmail.com>
2021-05-29 00:29:51 +0200

    excluded sessions and contenttypes instances from being loaded with loaddata

--- setup_database.sh
+++ setup_database.sh
@@ -2,5 +2,5 @@ python load_data.py
 cd backend
 ./manage.py makemigrations blog
 ./manage.py migrate
-./manage.py loaddata ../data.json --exclude sessions
+./manage.py loaddata --exclude sessions --exclude contenttypes --exclude admin ../data.json
 gunicorn backend.wsgi
