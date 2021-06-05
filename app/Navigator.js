4f7a3530916b554a2f3988800430c848ee703880
wasselbousmaha <wassel2005@gmail.com>
2021-06-05 17:12:04 +0200

    added tabs navigation in order to create a Search tab

--- app/Navigator.js
+++ app/Navigator.js
@@ -1,20 +1,39 @@
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Show from "./screens/Show";
 import ShowSingle from "./screens/ShowSingle";
+import Search from "./screens/Search";
 
 const Stack = createStackNavigator();
+const SearchNavigator = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+function LastArticlesStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Derniers articles" component={Show}/>
+            <Stack.Screen name="Article" component={ShowSingle}/>
+        </Stack.Navigator>
+    )
+}
+
+function SearchStack() {
+    return (
+        <SearchNavigator.Navigator>
+            <SearchNavigator.Screen name="Recherche" component={Search} />
+            <SearchNavigator.Screen name="Article" component={ShowSingle} />
+        </SearchNavigator.Navigator>
+    )
+}
 
 export default function Navigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <>
-                    <Stack.Screen name="Citizen" component={Show}/>
-                    <Stack.Screen name="Article" component={ShowSingle}/>
-                </>
-            </Stack.Navigator>
+            <Tabs.Navigator>
+                <Tabs.Screen name={"Derniers articles"} component={LastArticlesStack}/>
+                <Tabs.Screen name="Recherche" component={SearchStack} />
+            </Tabs.Navigator>
         </NavigationContainer>
     );
 }
